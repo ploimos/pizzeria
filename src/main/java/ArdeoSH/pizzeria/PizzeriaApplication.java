897e28c89217950682f7288ac96c1d6828aa27a7
Marco Plomitallo <m.plomitallo@outlook.com>
2025-07-23 13:13:24 +0200

    Revert "Pizzeria v2.0"
    This reverts commit fe9b17b8e9b3feb113aaba652a83c1c57c7647ef.

--- src/main/java/ArdeoSH/pizzeria/PizzeriaApplication.java
+++ src/main/java/ArdeoSH/pizzeria/PizzeriaApplication.java
@@ -13,6 +13,5 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class PizzeriaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PizzeriaApplication.class, args);
-
 	}
 }
